55c6e116dbacaad75741f5172028db98ec88f22c
sampletest2019 <sampletest2019@gmail.com>
2022-05-19 22:33:25 -0400

    test_rickandmorty_count_and_pages.py fixed. runConfiguration saved as files. minor cleanup.

--- tests/rick_and_morty_api/test_rickandmorty_count_and_pages.py
+++ tests/rick_and_morty_api/test_rickandmorty_count_and_pages.py
@@ -8,5 +8,5 @@ def test_validate_get_response_code():
     assert res.status_code == 200
     assert res.headers["Content-Type"] == "application/json; charset=utf-8"
     json_data = res.json()
-    assert json_data['info']['count'] == 26
+    assert json_data['info']['count'] == 29
     assert json_data['info']['pages'] == 2
